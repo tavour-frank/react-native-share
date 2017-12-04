844b3c75319f5b79212f0574b331cf0942ff03eb
frank <frank@tavour.com>
2017-12-04 14:29:04 -0800

    sms ios

--- ios/SmsShare.h
+++ ios/SmsShare.h
@@ -1,12 +1,3 @@
-//
-//  EmailShare.h
-//  RNShare
-//
-//  Created by Diseño Uno BBCL on 23-07-16.
-//  Copyright © 2016 Facebook. All rights reserved.
-//
-
-
 #import <UIKit/UIKit.h>
 // import RCTConvert
 #if __has_include(<React/RCTConvert.h>)
@@ -49,7 +40,7 @@
 #import "React/RCTUtils.h"   // Required when used as a Pod in a Swift project
 #endif
 #import <MessageUI/MessageUI.h>
-@interface EmailShare : NSObject <MFMailComposeViewControllerDelegate>
+@interface SmsShare : NSObject <MFMailComposeViewControllerDelegate>
 
 - (void *) shareSingle:(NSDictionary *)options failureCallback:(RCTResponseErrorBlock)failureCallback successCallback:(RCTResponseSenderBlock)successCallback;
 @end
